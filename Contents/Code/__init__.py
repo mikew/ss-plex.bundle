310c1c0590695e9628f090d7eecc5b4bc52e6241
Mike Wyatt <wyatt.mike@gmail.com>
2012-10-11 00:16:17 -0230

    initial favorite support

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -25,25 +25,86 @@ def Start():
 @handler(PLUGIN_PREFIX, PLUGIN_TITLE)
 def MainMenu():
     """docstring for MainMenu"""
-    return render_listings(listings_endpoint('/'))
+    container     = render_listings(listings_endpoint('/'))
+    favorite_item = DirectoryObject(
+        title = 'Favorites',
+        key   = Callback(Favorites)
+    )
 
+    container.add(favorite_item)
+    return container
+
+def has_dict(key):
+    """docstring for has_dict"""
+    if Dict[key]:
+        return True
+    else:
+        return False
+
+def show_is_favorite(show_title):
+    """docstring for show_is_favorite"""
+    return show_title in dict_default('favorites', {}).keys()
+
+def dict_default(key, default = None):
+    """docstring for dict_default"""
+    if has_dict(key):
+        return Dict[key]
+    else:
+        return default
+
+def Favorites():
+    """docstring for Favorites"""
+    favorites = dict_default('favorites', {})
+    container = ObjectContainer(
+        title1 = 'Favorites'
+    )
+
+    for show_title, url in favorites.items():
+        list_item = DirectoryObject(
+            title = show_title,
+            key   = Callback(ListTVShow, url = url, show_title = show_title)
+        )
+
+        container.add(list_item)
+
+    return container
 
 def RenderListings(url, default_title = None):
     """docstring for RenderListings"""
     return render_listings(url, default_title)
 
+def ListTVShow(url, show_title):
+    """docstring for ListTVShow"""
+    container     = render_listings(url, show_title)
+    if show_is_favorite(show_title):
+        favorite_label = '- Remove from Favorites'
+    else:
+        favorite_label = '+ Add to Favorites'
+
+    favorite_item = DirectoryObject(
+        title = favorite_label,
+        key   = Callback(ToggleFavorite, url = url, show_title = show_title)
+    )
+
+    container.objects.insert(0, favorite_item)
+    return container
+
+def ToggleFavorite(url, show_title):
+    """docstring for AddFavorite"""
+    if show_is_favorite(show_title):
+        del Dict['favorites'][show_title]
+    else:
+        favorites             = dict_default('favorites', {})
+        favorites[show_title] = url
+        Dict['favorites']     = favorites
+
+    #Dict['favorites'].save()
+
+    return ListTVShow(url, show_title)
+
 def ListSources(url, title):
     """docstring for SSListSources"""
     return render_listings(listings_endpoint('/sources?url=%s') % String.Quote(url), default_title = title)
-
-#def WatchLater(url):
-    #"""docstring for SSWatchLater"""
-    #container = ObjectContainer(header = 'Watch Later', message = 'Check on something else')
-    #values    = { 'download[url]': url }
-
-    #HTTP.Request(SS_URL_QUEUE, values, cacheTime = 0)
-
-    #return container
 
 def render_listings(url, default_title = None):
     """docstring for _render_listings"""
@@ -59,7 +120,7 @@ def render_listings(url, default_title = None):
         permalink        = element.get('url')
         display_title    = element.get('display_title') or element.get('title')
         generic_callback = Callback(RenderListings, url = permalink, default_title = display_title)
-        sources_callback = Callback(ListSources, url = permalink, title = display_title)
+        sources_callback = Callback(ListSources,    url = permalink, title         = display_title)
 
         if 'endpoint' == element['_type']:
             Log('element is endpoint')
@@ -75,7 +136,7 @@ def render_listings(url, default_title = None):
                 rating_key = permalink,
                 title      = display_title,
                 summary    = element.get( 'desc' ),
-                key        = generic_callback
+                key        = Callback(ListTVShow, url = permalink, show_title = display_title)
             )
         elif 'movie' == element['_type'] or 'episode' == element['_type']:
             Log('element is playable')
