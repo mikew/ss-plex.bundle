461bcf730bad48b21beed97f7ee35155764990b4
Mike Wyatt <wyatt.mike@gmail.com>
2013-03-03 15:22:57 -0330

    improves ssp directory detection

--- Contents/Libraries/Shared/bridge/plex.py
+++ Contents/Libraries/Shared/bridge/plex.py
@@ -11,31 +11,35 @@ def user_dict():   return config['Dict']
 def user_prefs():  return config['Prefs']
 def platform_os(): return config['Platform'].OS
 
-def section_element(section):
-    query = '//Directory[@type="%s"]' % section
-    dirs  = config['XML'].ElementFromURL('http://127.0.0.1:32400/library/sections').xpath(query)
-    for d in dirs:
-        if '.none' not in d.get('agent'):
-            return d
+def plex_endpoint(path): return 'http://127.0.0.1:32400%s' % path
 
-def refresh_section(section):
-    element = section_element(section)
-    key     = element.get('key')
-    url     = 'http://127.0.0.1:32400/library/sections/%s/refresh' % key
+def section_info(section):
+    import re
 
-    config['HTTP'].Request(url, immediate = True)
+    xmlobj   = config['XML'].ElementFromURL(plex_endpoint('/library/sections'))
+    query    = '//Directory[@type="%s"]' % section
+    matching = filter(lambda el: '.none' not in el.get('agent'), xmlobj.xpath(query))
+
+    for directory in matching:
+        locations = directory.xpath('./Location')
+        for location in locations:
+            path = location.get('path')
+            if re.search(r'ss.?p', path):
+                return match_from(directory, location)
+    else:
+        return match_from(directory, locations[0])
+
+def match_from(directory, location):
+    return [ directory.get('key'), location.get('path') ]
 
 def section_destination(section):
-    element   = section_element(section)
-    locations = element.xpath('./Location')
-    hinted    = locations[0].get('path')
-    fragment  = '/ssp'
+    found = section_info(section)
+    if not found: return
 
-    for element in locations:
-        path = element.get('path')
+    return found[1]
 
-        if path.endswith(fragment) or path.endswith(fragment + '/'):
-            hinted = path
-            break
+def refresh_section(section):
+    found = section_info(section)
+    if not found: return
 
-    return hinted
+    config['HTTP'].Request(plex_endpoint('/library/sections/%s/refresh' % found[0]), immediate = True)
