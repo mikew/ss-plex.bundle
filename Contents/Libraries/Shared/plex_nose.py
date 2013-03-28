f29e6c4c8d1d4a3e547644f8ac227867602f312b
Mike Wyatt <wyatt.mike@gmail.com>
2013-03-27 23:23:24 -0230

    adds latest plex_nose

--- Contents/Libraries/Shared/plex_nose.py
+++ Contents/Libraries/Shared/plex_nose.py
@@ -1,7 +1,27 @@
+from functools import wraps
+
 core = None
 
-def sandbox(f):
-    import nose
+def publish_local_file(local_path, name = None):
+    import os
 
-    core.sandbox.publish_api(nose)
-    core.sandbox.execute(f.func_code)
+    local_path = os.path.abspath(__file__ + '/../../../../' + local_path)
+    local_file = open(local_path, 'r')
+    contents   = local_file.read()
+    local_file.close()
+
+    if not name: name = os.path.basename(local_path)
+    core.sandbox.publish_api(contents, name = name)
+
+def sandbox(f):
+
+    @wraps(f)
+    def wrapper():
+        import nose
+
+        core.sandbox.publish_api(nose)
+        core.sandbox.publish_api(nose.tools.eq_)
+        core.sandbox.publish_api(nose.tools.ok_)
+        core.sandbox.execute(f.func_code)
+
+    return wrapper
