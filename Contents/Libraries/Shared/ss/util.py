f84615467025ec65bdbe36469d97793fbed2b145
Mike Wyatt <wyatt.mike@gmail.com>
2013-04-30 20:59:31 -0230

    v0.5.3

--- Contents/Libraries/Shared/ss/util.py
+++ Contents/Libraries/Shared/ss/util.py
@@ -167,5 +167,5 @@ def sorted_by_title(collection, getter = lambda x: x):
 class version:
     major  = 0
     minor  = 5
-    patch  = 2
+    patch  = 3
     string = '%s.%s.%s' % (major, minor, patch)
