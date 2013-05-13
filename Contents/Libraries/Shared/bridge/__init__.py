385f098e7c98ab29fa0b26de66a93f92d1b53a10
Mike Wyatt <wyatt.mike@gmail.com>
2013-05-13 02:30:08 -0230

    remove bridge, ss

--- Contents/Libraries/Shared/bridge/__init__.py
+++
@@ -1,5 +0,0 @@
-import plex
-import download
-import user
-import search
-import favorite
--- Contents/Libraries/Shared/ss/__init__.py
+++
@@ -1,6 +0,0 @@
-import util
-import cache
-from consumer       import Consumer
-from downloader     import Downloader
-from downloadstatus import DownloadStatus
-from wizard         import Wizard
