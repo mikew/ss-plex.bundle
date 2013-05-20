dd4deb0e349a49920f0bb145b71bc92f2a671e68
Mike Wyatt <wyatt.mike@gmail.com>
2013-05-20 05:57:42 -0230

    fix ss being setup multiple times

--- Contents/Libraries/Shared/ss/environment.py
+++ Contents/Libraries/Shared/ss/environment.py
@@ -1,4 +1,4 @@
-from _default import environment as _default_environment
+from _default import environment as default_environment
 
 def css_from_string(string, selector):
     return factory.css_from_string(string, selector)
@@ -15,4 +15,4 @@ def json_from_string(string):
 def json_from_url(url, params = {}, expires = 0):
     return factory.json_from_url(url, params = params, expires = expires)
 
-factory = _default_environment
+factory = default_environment
