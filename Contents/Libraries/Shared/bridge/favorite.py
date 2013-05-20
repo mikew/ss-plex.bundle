6d6c40d01894462b50bee04ac8cbf6f7b445c5cb
Mike Wyatt <wyatt.mike@gmail.com>
2013-05-20 20:29:14 -0230

    latest bridge

--- Contents/Libraries/Shared/bridge/favorite.py
+++ Contents/Libraries/Shared/bridge/favorite.py
@@ -22,7 +22,34 @@ def show_id_from_endpoint(endpoint):
     if matches:
         return matches.group(1)
 
+def show_ids():
+    ids = []
+
+    for endpoint, favorite in collection().iteritems():
+        id = show_id_from_endpoint(endpoint)
+        ids.append(id)
+
+    return ids
+
 def normalize_show_endpoint(endpoint):
     _id = show_id_from_endpoint(endpoint)
     if _id:
         return '/shows/%s' % _id
+
+def sync():
+    import ss
+
+    _collection   = collection()
+    sync_endpoint = ss.util.listings_endpoint('/sync')
+    params        = dict(ids = show_ids())
+    payload       = ss.environment.json_from_url(sync_endpoint, params = params)
+
+    for fav in payload:
+        if fav['endpoint'] in _collection:
+            existing = _collection[fav['endpoint']]
+
+            existing['title']    = fav.get('display_title')
+            existing['overview'] = fav.get('display_overview')
+            existing['artwork']  = fav.get('artwork')
+
+    settings.persist()
