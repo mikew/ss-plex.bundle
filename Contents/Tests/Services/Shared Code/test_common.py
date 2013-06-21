bec78f607bbf9ae4d5d7e167c100b3cc4a6206e4
Mike Wyatt <wyatt.mike@gmail.com>
2013-06-21 16:44:15 -0230

    simplify metadata on play screen

--- Contents/Tests/Services/Shared Code/test_common.py
+++ Contents/Tests/Services/Shared Code/test_common.py
@@ -2,46 +2,45 @@ import plex_nose
 
 class TestSsEnvironment(plex_nose.TestCase):
     def test_works():
-        ok_(isinstance(ss.environment.factory, common.SSPlexEnvironment))
+        eq_(ss.environment.factory.__class__.__name__, common.SSPlexEnvironment.__name__)
 
 class TestMetadata(plex_nose.TestCase):
-    def test_with_episode_numbered():
+    def test_with_episode():
         numbered_episode = dict(
-                display_title = 'foo 1x1 show title',
+                display_title = 'foo 1x1 episode title',
                 display_overview = u'2013-01-01 \u2014 an overview',
                 _type = 'episode')
 
         obj = common.metadata_from(numbered_episode)
 
-        eq_(obj.show, 'foo')
-        eq_(obj.season, 1)
-        eq_(obj.title, '1. show title')
+        ok_(obj.__class__ == EpisodeObject)
+        eq_(obj.title, 'foo 1x1 episode title')
         eq_(obj.summary, 'an overview')
         eq_(obj.originally_available_at, Datetime.ParseDate('2013-01-01').date())
 
-    def test_with_episode_daily():
-        numbered_episode = dict(
-                display_title = 'foo: show title 01.01.2013',
-                display_overview = u'2013-01-01 \u2014 an overview',
-                _type = 'episode')
+    #def test_with_episode_daily():
+        #numbered_episode = dict(
+                #display_title = 'foo: show title 01.01.2013',
+                #display_overview = u'2013-01-01 \u2014 an overview',
+                #_type = 'episode')
 
-        obj = common.metadata_from(numbered_episode)
+        #obj = common.metadata_from(numbered_episode)
 
-        eq_(obj.show, 'foo')
-        eq_(obj.title, 'show title')
-        eq_(obj.summary, 'an overview')
-        eq_(obj.originally_available_at, Datetime.ParseDate('2013-01-01').date())
+        #eq_(obj.show, 'foo')
+        #eq_(obj.title, 'show title')
+        #eq_(obj.summary, 'an overview')
+        #eq_(obj.originally_available_at, Datetime.ParseDate('2013-01-01').date())
 
-    def test_with_episode_subpar():
-        subpar_episode = dict(
-                display_title = 'foo',
-                display_overview = 'an overview',
-                _type = 'episode')
+    #def test_with_episode_subpar():
+        #subpar_episode = dict(
+                #display_title = 'foo',
+                #display_overview = 'an overview',
+                #_type = 'episode')
 
-        obj = common.metadata_from(subpar_episode)
+        #obj = common.metadata_from(subpar_episode)
 
-        eq_(obj.title, 'foo')
-        eq_(obj.summary, 'an overview')
+        #eq_(obj.title, 'foo')
+        #eq_(obj.summary, 'an overview')
 
     def test_with_movie():
         movie = dict(
